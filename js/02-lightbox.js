829b75409cdd51dffbc1404f7e4c21fda2604791
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2022-12-12 17:37:22 +0100

    added second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const creatItemsEl = (galleryItems) =>
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href='${original}'>
+         <img class="gallery__image"
+           src='${preview}'
+           alt='${description}' 
+         />
+       </a>`
+    )
+    .join("");
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", creatItemsEl(galleryItems));
+
+new SimpleLightbox("ul.gallery a", {
+  overlayOpacity: 0.9,
+  captionsData: "alt",
+  captionDelay: 250,
+});
