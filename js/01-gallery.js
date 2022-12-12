acc94e796c723bd0e67afffda38a953c59ee82cd
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2022-12-12 14:46:08 +0100

    added first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+//Створення і рендер розмітки
+
+const creatItemsEl = (galleryItems) =>
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src='${preview}'
+    data-source='${original}'
+    alt='${description}'
+  />
+</a>
+</div>`
+    )
+    .join("");
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", creatItemsEl(galleryItems));
+
+
+
+//Реалізація делегування на div.gallery
+gallery.addEventListener("click", onGalleryClick);
+
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  
+  if (!e.target.classList.contains(`gallery__image`)) {  
+    // если элемент не содержит данного класса то выход
+    return;
+  };
+
+  const bigImageUrl = e.target.dataset.source;
+
+  const modal = basicLightbox.create(`
+    <img src = "${bigImageUrl}" width = "100%" height = "100%"/>`,
+    {
+      onShow: () => {window.addEventListener('keydown', onEscKeyPress);},
+      onClose: () => {window.removeEventListener('keydown', onEscKeyPress);},
+    }
+  );
+
+  modal.show();
+
+  function onEscKeyPress(evt) {
+    if (evt.code === 'Escape') {
+     modal.close();
+    }
+    };
+};
+
